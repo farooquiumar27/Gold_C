3c06fbe759487e4def2a63ac013846a933432a08
farooquiumar27 <farooquiumar27@gmail.com>
2023-11-21 13:09:47 +0530

    Phase one of Gold

---
+++ build/CMakeFiles/goldC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goldC.dir/src/main.cpp.o"
+  "CMakeFiles/goldC.dir/src/main.cpp.o.d"
+  "goldC"
+  "goldC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goldC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
